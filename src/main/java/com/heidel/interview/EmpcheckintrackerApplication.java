42147dcfef2ba84d276ab4d0dae102426dcc5047
Jusbin Mathew <jusbin.mathew@inovate.inovate.com>
2020-02-15 20:30:39 +0530

    Initial commit

---
+++ src/main/java/com/heidel/interview/EmpcheckintrackerApplication.java
@@ -0,0 +1,13 @@
+package com.heidel.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpcheckintrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpcheckintrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heidel/interview/EmpcheckintrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heidel.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpcheckintrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
