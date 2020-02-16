abe056cd015fc610cb50854b4828316f1283e48d
Jusbin Mathew <jusbin.mathew@inovate.inovate.com>
2020-02-16 18:12:34 +0530

    Updated with ui changes

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+
     $('#employee-info-list').DataTable();
+
+    //open modal on click of icon in the row.
+
+
+
+    $('#employee-info-list').DataTable();
+
+
+
+
 } );
+
+function openEditEmpModal(empId)
+{
+
+$("#emp-edit-checking-modal").modal()
+}
+
+function submitDetails()
+{
+
+}
